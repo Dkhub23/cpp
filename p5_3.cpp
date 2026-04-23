0ba0f3f2ca00c8516a99ce75ea5633a9a1e6fdcf
Dkhub23 <25cs025@charusat.edu.in>
2026-04-23 18:41:15 +0530

    add files

--- p5_3.cpp
+++ p5_3.cpp
@@ -1,7 +1,58 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+
 using namespace std;
 
 int main(){
+    string name[100],s;
+    int quantity[100],n;
+    int price[100];
+    ofstream    d1("inventry.txt");
+
+    cout<<"enter total number of item:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+
+    cout<<"name:";
+    cin>>name[i];
+    cout<<"quantity:";
+    cin>>quantity[i];
+    cout<<"price:";
+    cin>>price[i];
+    }
+
+    d1<<"name"<<"     "<<"quantity"<<"          "<<"price"<<endl;
+   
+    for(int i=0;i<n;i++){
+    d1<<name[i]<<"                "<<quantity[i]<<"                    "<<price[i]<<endl;
+    
+    
+    }
+    d1.close();
+    ifstream d2("inventry.txt");
+    string ans,searchName;
+
+    cout<<"do you want to search:";
+    cin>>ans;
+    if(ans=="yes")
+    {
+        cout<<"enter name to search:";
+        cin>>searchName;
+        while(d2>>s)
+        {
+            if(s==searchName)
+            {
+                cout<<"Item found!"<<endl;
+                break;
+            }
+            else{
+            cout<<"Item not found!"<<endl;
+            break;
+            }
+
+        }
+    }
 
 return 0;
 }
